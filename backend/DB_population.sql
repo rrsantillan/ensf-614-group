a20a79fec6cc80c75e74efcef9b34cd09e8f2908
BradenTink <71302948+BradenTink@users.noreply.github.com>
2023-11-23 17:52:21 -0700

    Update DB_population.sql

--- backend/DB_population.sql
+++ backend/DB_population.sql
@@ -2,22 +2,23 @@ DROP DATABASE IF EXISTS ensf_614;
 CREATE DATABASE ensf_614;
 USE ensf_614;
 
-DROP TABLE IF EXISTS USERTABLE;
+DROP TABLE IF EXISTS usertable;
 CREATE TABLE usertable (
 	USERNAME			varchar(25) NOT null,
 	PASSWORD			varchar(25) NOT null,
-	EMAIL				varchar(25),
-	DOB				varchar(25),				
+	PROFILE 			varchar(25) NOT NULL,
+	EMAIL				varchar(25) NOT NULL,
+	DOB				TIMESTAMP,				
 	primary key (USERNAME)
 );
 		
-DROP TABLE IF FLIGHTS EXISTS ;
+DROP TABLE IF EXISTS FLIGHTS;
 CREATE TABLE flights (    
 	FLIGHTID			varchar(25) NOT null,
 	SOURCE			varchar(25) NOT null,
 	DESTINATION		varchar(25) NOT null,
-	DEPARTURE		varchar(25),
-	LANDING 			varchar(25),				
+	DEPARTURE		TIMESTAMP,
+	LANDING 			TIMESTAMP,				
 	primary key (FLIGHTID)  
 );
 
@@ -26,8 +27,7 @@ CREATE TABLE REGISTEREDTICKETS (
 	USERNAME			varchar(25) NOT null,
 	FLIGHTID			varchar(25) NOT null,
 	SEATNUMBER			varchar(25) NOT null,
-	primary key (USERNAME),
-	primary key (SEATNUMBER)  
+	primary key (FlightID, SEATNUMBER)  
 );
 
 DROP TABLE IF EXISTS CREW; 
@@ -35,63 +35,55 @@ CREATE TABLE CREW (
 	FNAME			varchar(25) NOT null,
 	LASTNAME			varchar(25) NOT null,
 	CREWID			varchar(25) NOT null,
-	primary key (CREWID),
+	primary key (CREWID)
 );
 
 DROP TABLE IF EXISTS ASSIGNEDCREW; 
 CREATE TABLE ASSIGNEDCREW (
 	FLIGHTID			varchar(25) NOT null,
 	CREWID			varchar(25) NOT null,
-	primary key (FLIGHTID),
 	primary key (CREWID)  
 );
 
-DROP TABLE IF EXISTS CREW; 
-CREATE TABLE CREW (
-	FNAME			varchar(25) NOT null,
-	LASTNAME			varchar(25) NOT null,
-	ID			varchar(25) NOT null,
-	primary key (USERNAME),
-	primary key (SEATNUMBER)  
-);
 
 
-INSERT INTO USERTABLE (USERNAME, PASSWORD, EMAIL, DOB)
+
+INSERT INTO USERTABLE (USERNAME, PASSWORD, PROFILE, EMAIL, DOB)
 VALUES
-('Braden', 'Tink19', 'braden@gmail.com',	'1969-10-17'),
-('Fred', 'Joe434', 'fred@gmail.com',	'1979-04-19'),
-('Bob', 'JOeblow', 'bob@gmail.com',	'1999-09-12');
+('Braden', '1234', 'Agent', 'braden@gmail.com',	'1996-10-17'),
+('Fred', 'fred', 'Admin', 'fred@gmail.com',	'1990-04-19'),
+('Bob', 'bob', 'REGUSER', 'bob@gmail.com',	'1999-09-12');
 
 
 INSERT INTO FLIGHTS (FLIGHTID, SOURCE, DESTINATION, DEPARTURE, LANDING)
 VALUES
-('Braden', 'Tink19', 'braden@gmail.com',	'1969-10-17'),
-('Fred', 'Joe434', 'fred@gmail.com',	'1979-04-19'),
-('Bob', 'JOeblow', 'bob@gmail.com',	'1999-09-12');
+('1', 'YYC', 'YVR',	'2023-11-12', '2023-11-13'),
+('2', 'YYC', 'YVR',	'2023-11-12', '2023-11-14'),
+('3', 'YYC', 'YVR',	'2023-11-13', '2023-11-16');
 
-INSERT INTO registeredtickets(USERNAME, FLIGHTID, SEATNUMBER)
+INSERT INTO registeredtickets (USERNAME, FLIGHTID, SEATNUMBER)
 VALUES
-('Braden', 1, 'A1'),
-('Guest', 1, 'B1'),
-('Guest', 1, 'A3'),
-('Guest', 1, 'C1'),
-('Guest', 1, 'D4');
+('Braden', '1', 'A1'),
+('Guest', '1', 'B1'),
+('Guest', '1', 'A3'),
+('Guest', '1', 'C1'),
+('Guest', '1', 'D4');
 
-INSERT INTO ASSIGNEDCREW(FLIGHTID, CREWID)
-VALUES
-( 1, 'A001'),
-( 1, 'A002'),
-( 1, 'A003'),
-( 1, 'A004'),
-( 1, 'A005');
 
-INSERT INTO CREW(FNAME, LASTNAME, ID)
+
+
+INSERT INTO CREW(FNAME, LASTNAME, CREWID)
 VALUES
 ('Larry', 'Bird', 'A001'),
 ('Greg', 'Bird', 'A002'),
 ('Jon', 'Smith', 'A003'),
 ('Nancy', 'Drew', 'A004'),
-('Bob', 'Tim', 'A005'),
+('Bob', 'Tim', 'A005');
+
+INSERT INTO ASSIGNEDCREW(FLIGHTID, CREWID)
+VALUES
+('1', 'A001'),
+('1', 'A002'),
 
 
 
