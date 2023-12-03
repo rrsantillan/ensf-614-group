5f039be3a96c9ce00cff3f3df78ebe484910306c
rrsantillan <132472698+rrsantillan@users.noreply.github.com>
2023-12-03 14:58:11 -0700

    Updated Timestamps in menus. Retool visuals.

--- frontend/src/Components/Agent/Agentview.js
+++ frontend/src/Components/Agent/Agentview.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react'
 import { useParams, useNavigate } from 'react-router-dom';
 import Header from '../Header'; 
 import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import moment from 'moment'
 
 
 function Agentview(){
@@ -28,7 +29,17 @@ function Agentview(){
        
         
     };
- 
+    
+    // const formattedDate = moment.utc(flight.DEPARTURETIME).format('YYYY-MM-DDTHH:mm:ss.SSS')
+    // moment().format('MMMM Do YYYY, h:mm:ss a')
+
+    const formatFlightTime = (data) => {
+        return data.map(flight => ({
+          ...flight,
+          DEPARTURETIME: moment.utc(flight.DEPARTURETIME).format('MMMM Do YYYY, h:mm a'),
+          ARRIVALTIME: moment.utc(flight.ARRIVALTIME).format('MMMM Do YYYY, h:mm a')
+        }));
+    };
 
     const handleSumbit = (event)=> {
         event.preventDefault();
@@ -37,7 +48,8 @@ function Agentview(){
         axios.post('http://localhost:8081/agent/getAllFlights')
         .then((res) => {
             const fetchedFlightData = res.data.flights;
-            setFlightData(fetchedFlightData);
+            const fetchedFlightDataTimeFormatted = formatFlightTime(fetchedFlightData)
+            setFlightData(fetchedFlightDataTimeFormatted);
         })
         .catch((err) => {
             console.error(err);       
@@ -48,11 +60,11 @@ function Agentview(){
         
         <div className="p-0 container-fluid">
             <Header className="" Profile1={Profile1} username={username}/>{/* Assuming Header is your component for the header */}
-            <div className="d-flex vh-90 justify-content-center align-items-top">
+            <div className="my-3 d-flex vh-90 justify-content-center align-items-top">
                 <div className='p-3 bg-light w-75'>
-                    <h2>Browse Flights</h2>
+                    <h2>See passenger lists:</h2>
                     <form action='' onSubmit={handleSumbit}>
-                        <button type='submit' className='btn btn-success w-100'>Get Flights</button>
+                        <button type='submit' className='btn btn-success w-100'>Show all flights</button>
                     </form>
                     {Array.isArray(flightData) && flightData.length > 0 && (
                         <div className="mt-5">
@@ -60,8 +72,8 @@ function Agentview(){
                             <div style={{ maxHeight: '300px', overflowY: 'auto' }}>
                                 {flightData.map((flight, index) => (
                                     <div className="flight-data-container" key={index}>
-                                        <p>Origin: {flight.ORIGIN} Departure Time: {flight.DEPARTURETIME}</p>
-                                        <p>Destination: {flight.DESTINATION} Landing Time: {flight.ARRIVALTIME}</p>
+                                        <p><strong>Origin:</strong> {flight.ORIGIN} <strong>Departure Time:</strong> {flight.DEPARTURETIME}</p>
+                                        <p><strong>Destination:</strong> {flight.DESTINATION} <strong>Landing Time:</strong> {flight.ARRIVALTIME}</p>
                                         
                                         <button onClick={() => 
                                             {
